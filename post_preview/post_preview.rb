6fe8176100638bedb79ccaec7b51400d75000485
Brian McMurray <brian@brianmcmurray.com>
2011-11-15 18:54:53 -0500

    Start modifying post_preview to allow truncate on break character or maximum word length.

--- post_preview/post_preview.rb
+++ post_preview/post_preview.rb
@@ -1,15 +1,19 @@
 module Jekyll
-
   class Post
     def preview_content
       delimeter = @site.config['content_delimeter'] || '<!-- -**-END-**- -->'
-      self.content.split(delimeter)[0]
+      if self.content.length >= (@site.config['preview_minimum_length'] || 250)
+        self.content.split(delimeter)[0]
+      end
     end
   end
-  
+
   AOP.around(Post, :to_liquid) do |post_instance, args, proceed, abort|
     result = proceed.call
-    result['preview'] = post_instance.preview_content
+    if post_instance.preview_content && !post_instance.preview_content.gsub(/\n/, '').strip.empty?
+      result['preview'] = post_instance.preview_content
+    end
     result
   end
 end
+
